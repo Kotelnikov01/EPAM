46342ebf6031e8345e57d7f1533a00c2c1d0c90e
Alexandr <AksHD@mail.ru>
2018-01-04 23:33:52 +0300

    framework

---
+++ Framework/Framework/Pages/MainPage.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace Framework.Pages
+{
+    public class MainPage
+    {
+        private const string BASE_URL = "https://www.alitalia.com/en_en";
+
+        [FindsBy(How = How.Id, Using = "departureInput")]
+        private IWebElement departureInput;
+
+        [FindsBy(How = How.Id, Using = "arrivalInput")]
+        private IWebElement arrivalInput;
+
+        [FindsBy(How = How.Id, Using = "andata")]
+        private IWebElement andata;
+
+        [FindsBy(How = How.Id, Using = "ritorno")]
+        private IWebElement ritorno;
+       
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"cercaVoliForm\"]/div/fieldset[4]/ol/li[1]/div/a[1]")]
+        private IWebElement buttonAdult;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"cercaVoliForm\"]/div/fieldset[4]/ol/li[1]/div/a[2]")]
+        private IWebElement buttonAdultMinus;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"cercaVoliForm\"]/div/fieldset[4]/ol/li[2]/div/a[1]")]
+        private IWebElement buttonChildren;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"cercaVoliForm\"]/div/fieldset[4]/ol/li[3]/div/a[1]")]
+        private IWebElement buttontInfant;
+
+        [FindsBy(How = How.Id, Using = "cercaVoliSubmit")]
+        private IWebElement SearchButton;
+
+        
+
+        
+
+
+        private IWebDriver driver;
+
+        public MainPage(IWebDriver driver)
+        {
+
+            this.driver = driver;
+            PageFactory.InitElements(this.driver, this);
+        }
+
+        public void OpenPage()
+        {
+            driver.Navigate().GoToUrl(BASE_URL);
+        }
+
+        public void ClickButton()
+        {
+            SearchButton.Click();
+        }
+
+
+
+        public void Test1(string origin, string destination, DateTime departDate, DateTime returnDate)
+        {
+            departureInput.SendKeys(origin);
+            departureInput.SendKeys(Keys.Enter);
+            arrivalInput.SendKeys(destination);
+            arrivalInput.SendKeys(Keys.Enter);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            andata.SendKeys(Convert.ToString(departDate));
+            andata.SendKeys(Keys.Enter);
+            ritorno.SendKeys(Convert.ToString(returnDate));
+            ritorno.SendKeys(Keys.Tab);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            buttonAdult.Click();
+            SearchButton.Click();
+        }
+
+        public void Test2(string origin, string destination, DateTime departDate, DateTime returnDate)
+        {
+            departureInput.SendKeys(origin);
+            departureInput.SendKeys(Keys.Enter);
+            arrivalInput.SendKeys(destination);
+            arrivalInput.SendKeys(Keys.Enter);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            andata.SendKeys(Convert.ToString(departDate));
+            andata.SendKeys(Keys.Enter);
+            ritorno.SendKeys(Convert.ToString(returnDate));
+            ritorno.SendKeys(Keys.Tab);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            buttonAdult.Click();
+            buttonChildren.Click();
+            buttontInfant.Click();
+            SearchButton.Click();
+        }
+
+        public void Test3(string origin, DateTime departDate, DateTime returnDate)
+        {
+            departureInput.SendKeys(origin);
+            departureInput.SendKeys(Keys.Enter);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            andata.SendKeys(Convert.ToString(departDate));
+            andata.SendKeys(Keys.Enter);
+            ritorno.SendKeys(Convert.ToString(returnDate));
+            ritorno.SendKeys(Keys.Tab);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            buttonAdult.Click();
+            SearchButton.Click();
+        }
+
+        public void Test4(string origin, string destination, DateTime departDate, DateTime returnDate, DateTime returnDate1)
+        {
+            departureInput.SendKeys(origin);
+            departureInput.SendKeys(Keys.Enter);
+            arrivalInput.SendKeys(destination);
+            arrivalInput.SendKeys(Keys.Enter);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            andata.SendKeys(Convert.ToString(departDate));
+            andata.SendKeys(Keys.Enter);
+            ritorno.SendKeys(Convert.ToString(returnDate));
+            ritorno.SendKeys(Keys.Tab);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            buttonAdult.Click();
+            ritorno.Clear();
+            ritorno.SendKeys(Convert.ToString(returnDate1));
+            ritorno.SendKeys(Keys.Tab);
+            buttonAdultMinus.Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            SearchButton.Click();
+        }
+
+        public void Test5(string origin, DateTime departDate, DateTime returnDate)
+        {
+            departureInput.Clear();
+            departureInput.SendKeys(Keys.Enter);
+            arrivalInput.SendKeys(origin);
+            arrivalInput.SendKeys(Keys.Enter);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            andata.SendKeys(Convert.ToString(departDate));
+            andata.SendKeys(Keys.Enter);
+            ritorno.SendKeys(Convert.ToString(returnDate));
+            ritorno.SendKeys(Keys.Tab);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            buttonAdult.Click();
+            SearchButton.Click();
+        }
+
+        public bool IsTicketsListExist()
+        {
+            var ticketsList = driver.FindElements(By.XPath("//*[@id=\"booking - flight - select - selection\"]/div[10]/div[3]/div[1]"));
+            return ticketsList.Count > 0 ? true : false;
+        }
+
+        public bool IsErrorExist()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            return null != driver.FindElement(By.XPath("//*[@id=\"booking - flight - select - selection\"]/div[10]/div")) ? true : false;
+                                                        
+        }
+
+        public bool IsErrorDestination()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            return null != driver.FindElement(By.Id("Destination_error")) ? true : false;
+
+        }
+
+        public bool IsErrorOrigin()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            return null != driver.FindElement(By.Id("Origin_error")) ? true : false;
+
+        }
+
+        public bool IsErrorData()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            return null != driver.FindElement(By.Id("ReturnDate_error")) ? true : false;
+
+        }
+
+    }
+
+
+
+}
+
---
+++ Framework/Framework/Tests/Test.cs
@@ -0,0 +1,144 @@
+﻿using Framework.CromeDriver;
+using Framework.Pages;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Framework.Tests
+{
+    /// <summary>
+    /// Сводное описание для Test
+    /// </summary>
+    [TestClass]
+    public class Test
+    {
+        public Test()
+        {
+            //
+            // TODO: добавьте здесь логику конструктора
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Получает или устанавливает контекст теста, в котором предоставляются
+        ///сведения о текущем тестовом запуске и обеспечивается его функциональность.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Дополнительные атрибуты тестирования
+        //
+        // При написании тестов можно использовать следующие дополнительные атрибуты:
+        //
+        // ClassInitialize используется для выполнения кода до запуска первого теста в классе
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // ClassCleanup используется для выполнения кода после завершения работы всех тестов в классе
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // TestInitialize используется для выполнения кода перед запуском каждого теста 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // TestCleanup используется для выполнения кода после завершения каждого теста
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var driver = DriverInstance.GetInstance();
+            MainPage page = new MainPage(driver);
+            page.OpenPage();
+            page.Test1("Berlin", "Paris", new DateTime(2018, 01, 04), new DateTime(2018, 01, 07));
+
+            Assert.IsTrue(page.IsTicketsListExist());
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var driver = DriverInstance.GetInstance();
+            MainPage page = new MainPage(driver);
+            page.OpenPage();
+            page.Test2("Berlin", "Paris", new DateTime(2018, 01, 04), new DateTime(2018, 01, 07));
+
+            Assert.IsTrue(page.IsTicketsListExist());
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var driver = DriverInstance.GetInstance();
+            MainPage page = new MainPage(driver);
+            page.OpenPage();
+            page.Test1("Berlin", "Paris", new DateTime(2018, 01, 04), new DateTime(2018, 01, 04));
+
+            Assert.IsTrue(page.IsErrorExist());
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var driver = DriverInstance.GetInstance();
+            MainPage page = new MainPage(driver);
+            page.OpenPage();
+            page.Test3("Berlin", new DateTime(2018, 01, 04), new DateTime(2018, 01, 07));
+
+            Assert.IsTrue(page.IsErrorDestination());
+
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var driver = DriverInstance.GetInstance();
+            MainPage page = new MainPage(driver);
+            page.OpenPage();
+            page.Test4("Berlin", "Paris", new DateTime(2018, 01, 04), new DateTime(2018, 01, 07), new DateTime(2018, 01, 02));
+
+            Assert.IsTrue(page.IsErrorData());
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            var driver = DriverInstance.GetInstance();
+            MainPage page = new MainPage(driver);
+            page.OpenPage();
+            page.Test1("Berlin", "Berlin", new DateTime(2018, 01, 04), new DateTime(2018, 01, 07));
+
+            Assert.IsTrue(page.IsErrorDestination());
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            var driver = DriverInstance.GetInstance();
+            MainPage page = new MainPage(driver);
+            page.OpenPage();
+            page.Test5("Berlin", new DateTime(2018, 01, 04),  new DateTime(2018, 01, 12));
+
+            Assert.IsTrue(page.IsErrorOrigin());
+
+        }
+
+        
+    }
+}
